3251efbce3949154d7d583d8760065cf30c6808c
christinex1111 <christinexu@Christines-MacBook-Pro.local>
2022-12-31 11:37:21 -0600

    new commit

---
+++ java/singleNumber.java
@@ -0,0 +1,26 @@
+//import java.security.KeyStore.Entry;
+import java.util.HashMap;
+
+public class singleNumber {
+    public static void main(String[] args) {
+        int nums[] = {1};
+        //int value = 1;
+        HashMap <Integer, Integer> numsMap= new HashMap<>();
+        //System.out.println(nums.length);
+
+        for (int i : nums) {
+            if (numsMap.get(i) == null) {
+                numsMap.put(i,1);
+            } else {
+                numsMap.put(i, numsMap.get(i)+1);
+            }
+        }
+        //System.out.println(numsMap);
+
+        for (java.util.Map.Entry<Integer, Integer> entry : numsMap.entrySet()) {
+            if (entry.getValue() == 1) {
+                System.out.println(entry.getKey());
+            }
+    }
+}
+}
