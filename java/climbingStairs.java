7c7e8748e60dac3907619032c4da9749058db097
christinex1111 <christinexu@Christines-MacBook-Pro.local>
2023-01-06 19:10:14 -0600

    new commit

--- java/climbingStairs.java
+++ java/climbingStairs.java
@@ -1,8 +1,18 @@
-import java.util.ArrayList;
-import java.util.List;
+//import java.util.ArrayList;
+//import java.util.List;
 
 public class climbingStairs {
+    public static int climbStairs(int n) {
+       if (n <= 2) {
+            return n;
+        } 
+
+        return climbStairs(n-2) + climbStairs(n-1);
+
+    }
+
     public static void main(String[] args) {
-        
+        int n = 5;
+        System.out.println(climbStairs(n));
     }
 }
